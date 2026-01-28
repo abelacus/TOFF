be74a482eac1c3ea4dc4cc4d0295f0391a77f3b9
abelacus <43807331+abelacus@users.noreply.github.com>
2026-01-28 10:04:41 +1000

    Fix getting file info on windows. turns out getting by filename is windows 11 only. whoops.

--- App/Services/FileInfoHandlers/FileInfoWindows.cs
+++ App/Services/FileInfoHandlers/FileInfoWindows.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
 using TOFF.Models;
 using TorrentClient.Models;
+using static Terminal.Gui.Drivers.WindowsConsole;
 
 namespace TOFF.Services.FileInfoHandlers
 {
@@ -25,28 +27,65 @@ namespace TOFF.Services.FileInfoHandlers
             public uint NumberOfLinks;
             public uint EffectveAccess;
         };
-
         enum _FILE_INFO_BY_NAME_CLASS //technically could support additional classes, but first one has everything we need
         {
             FileStatByNameInfo,
         }
 
+
+        //apparently only available in windows 11, whoops
         [DllImport("kernel32.dll", SetLastError = true)]
         private static extern bool GetFileInformationByName(string FileName, _FILE_INFO_BY_NAME_CLASS FileInformationClass, out FILE_STAT_INFORMATION FileInformation, uint Size);
 
+        [StructLayout(LayoutKind.Sequential)]
+        struct BY_HANDLE_FILE_INFORMATION
+        {
+            public uint dwFileAttributes;
+            public System.Runtime.InteropServices.ComTypes.FILETIME ftCreationTime;
+            public System.Runtime.InteropServices.ComTypes.FILETIME ftLastAccessTime;
+            public System.Runtime.InteropServices.ComTypes.FILETIME ftLastWriteTime;
+            public uint dwVolumeSerialNumber;
+            public uint nFileSizeHigh;
+            public uint nFileSizeLow;
+            public uint nNumberOfLinks;
+            public uint nFileIndexHigh;
+            public uint nFileIndexLow;
+        };
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool GetFileInformationByHandle(SafeFileHandle hFile, out BY_HANDLE_FILE_INFORMATION FileInformation);
+
+
         public FileInformation GetFileInfo(string filePath)
         {
-            GetFileInformationByName(filePath, _FILE_INFO_BY_NAME_CLASS.FileStatByNameInfo, out var info, (uint)Marshal.SizeOf<FILE_STAT_INFORMATION>());
-
-
-
-            return new FileInformation
+            try //almost certainly a better way to handle this but this is fine for now i guess.
             {
-                savePath = filePath,
-                creationDate = new DateTime((int)info.CreationTime),
-                lastModifiedDate = new DateTime((int)info.ChangeTime),
-                links = info.NumberOfLinks,
-            };
+                GetFileInformationByName(filePath, _FILE_INFO_BY_NAME_CLASS.FileStatByNameInfo, out var info, (uint)Marshal.SizeOf<FILE_STAT_INFORMATION>());
+
+                return new FileInformation
+                {
+                    savePath = filePath,
+                    creationDate = new DateTime((int)info.CreationTime),
+                    lastModifiedDate = new DateTime((int)info.ChangeTime),
+                    links = info.NumberOfLinks,
+                };
+            }
+            catch (Exception e) //will fail on windows 10 and earlier so need an alternative
+            {
+                SafeFileHandle handle = File.OpenHandle(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, FileOptions.None);
+
+                GetFileInformationByHandle(handle, out var info);
+
+                handle.Close();
+
+                return new FileInformation
+                {
+                    savePath = filePath,
+                    creationDate = DateTime.FromFileTime((((long)info.ftCreationTime.dwHighDateTime) << 32) | ((uint)info.ftCreationTime.dwLowDateTime)),
+                    lastModifiedDate = DateTime.FromFileTime((((long)info.ftLastWriteTime.dwHighDateTime) << 32) | ((uint)info.ftLastWriteTime.dwLowDateTime)),
+                    links = info.nNumberOfLinks
+                };
+            }
 
         }
     }
