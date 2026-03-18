1e5011d88de72eb01b88105619cf689b2ba7e533
abelacus <43807331+abelacus@users.noreply.github.com>
2026-03-19 09:07:00 +1000

    remove mono.unix dependancy

--- App/Services/FileInfoHandlers/FileInfoUnix.cs
+++ App/Services/FileInfoHandlers/FileInfoUnix.cs
@@ -1,20 +1,132 @@
-﻿using TOFF.Models;
-using Mono.Unix.Native;
+﻿using System.Runtime.InteropServices;
+using TOFF.Models;
 
 namespace TOFF.Services.FileInfoHandlers
 {
     internal class FileInfoUnix : IFileInfoHandler
     {
+        //taken from https://stackoverflow.com/questions/69777374/how-to-get-linux-file-permissions-in-net-5-net-6-without-mono-posix-with-p-i
+        [DllImport("libc", SetLastError = true)]
+        internal static extern int statx(int dirfd, string path, int flags, uint mask, out Statx data);
+
+        /// <summary>
+        /// POSIX statx data structure.
+        /// </summary>
+        internal struct Statx
+        {
+
+            /// <summary>
+            /// Mask of bits indicating filled fields.
+            /// </summary>
+            internal uint Mask;
+            /// <summary>
+            /// Block size for filesystem I/O.
+            /// </summary>
+            internal uint BlockSize;
+            /// <summary>
+            /// Extra file attribute indicators
+            /// </summary>
+            internal ulong Attributes;
+            /// <summary>
+            /// Number of hard links.
+            /// </summary>
+            internal uint HardLinks;
+            /// <summary>
+            /// User ID of owner.
+            /// </summary>
+            internal uint Uid;
+            /// <summary>
+            /// Group ID of owner.
+            /// </summary>
+            internal uint Gid;
+            /// <summary>
+            /// File type and mode.
+            /// </summary>
+            internal ushort Mode;
+            private ushort Padding01;
+            /// <summary>
+            /// Inode number.
+            /// </summary>
+            internal ulong Inode;
+            /// <summary>
+            /// Total size in bytes.
+            /// </summary>
+            internal ulong Size;
+            /// <summary>
+            /// Number of 512B blocks allocated.
+            /// </summary>
+            internal ulong Blocks;
+            /// <summary>
+            /// Mask to show what's supported in <see cref="Attributes"/>.
+            /// </summary>
+            internal ulong AttributesMask;
+            /// <summary>
+            /// Last access time.
+            /// </summary>
+            internal StatxTimeStamp AccessTime;
+            /// <summary>
+            /// Creation time.
+            /// </summary>
+            internal StatxTimeStamp CreationTime;
+            /// <summary>
+            /// Last status change time.
+            /// </summary>
+            internal StatxTimeStamp StatusChangeTime;
+            /// <summary>
+            /// Last modification time.
+            /// </summary>
+            internal StatxTimeStamp LastModificationTime;
+            internal uint RDevIdMajor;
+            internal uint RDevIdMinor;
+            internal uint DevIdMajor;
+            internal uint DevIdMinor;
+            internal ulong MountId;
+            private ulong Padding02;
+            private ulong Padding03;
+            private ulong Padding04;
+            private ulong Padding05;
+            private ulong Padding06;
+            private ulong Padding07;
+            private ulong Padding08;
+            private ulong Padding09;
+            private ulong Padding10;
+            private ulong Padding11;
+            private ulong Padding12;
+            private ulong Padding13;
+            private ulong Padding14;
+            private ulong Padding15;
+        }
+
+        /// <summary>
+        /// Time stamp structure used by statx.
+        /// </summary>
+        public struct StatxTimeStamp
+        {
+
+            /// <summary>
+            /// Seconds since the Epoch (UNIX time).
+            /// </summary>
+            public long Seconds;
+
+            /// <summary>
+            /// Nanoseconds since <see cref="Seconds"/>.
+            /// </summary>
+            public uint Nanoseconds;
+
+        }
+
         public FileInformation GetFileInfo(string filePath)
         {
-            Syscall.stat(filePath, out var info);
+            uint mask = 0x0004U & 0x0080U & 0x0040U & 0x0020U; //STATX_NLINK, STATX_CTIME, STATX_MTIME, STATX_ATIME. values from statx-generic.h
+
+            statx(0, filePath, 0, mask, out var info); //dirfd is 0 since filepaths should all be absolute. 
 
             return new FileInformation
             {
                 savePath = filePath,
-                creationDate = DateTimeOffset.FromUnixTimeSeconds(info.st_ctime).DateTime, //is actually last metadata modification time, but should be good enough. easier than using statx and worrying about whether the filesystem returns it
-                lastModifiedDate = DateTimeOffset.FromUnixTimeSeconds(info.st_mtime).DateTime,
-                links = (uint)info.st_nlink
+                creationDate = DateTimeOffset.FromUnixTimeSeconds(info.CreationTime.Seconds).DateTime,
+                lastModifiedDate = DateTimeOffset.FromUnixTimeSeconds(info.LastModificationTime.Seconds).DateTime,
+                links = info.HardLinks
             };
         }
     }
